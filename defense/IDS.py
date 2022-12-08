aaa188f4168bb71649cf391ee5cb66200f39f669
Jonathan Hsu <jhsu0903@hotmail.com>
2022-12-07 19:50:04 -0500

    Initial IDS test

--- defense/IDS.py
+++ defense/IDS.py
@@ -1,6 +1,26 @@
 from scapy.all import *
 from scapy.layers.inet import ICMP, IP
 import time
-import libpcap
+from multiprocessing import Process
+
+destIP = 'localhost'
+srcIP = 'localhost'
+interface = '\\Device\\NPF_Loopback'
+
+def processPacket(packet):
+    if ICMP in packet:
+        if Raw in packet:
+            data = packet[Raw].load.decode()
+            if "botreply" in data:
+                return
+            else:
+                send(packet)
+
+def sniffForPacket():
+    sniff(iface=interface, prn=processPacket, filter='icmp')
+
+if __name__ == '__main__':
+    p = Process(target=sniffForPacket)
+    p.start()
 
 
