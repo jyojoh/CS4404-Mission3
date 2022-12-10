46ff05ce2dd6b9461ababa2dd1f97b0a2f2a865a
hzgand <101759753+hzgand@users.noreply.github.com>
2022-12-09 19:25:05 -0500

    Update IDS.py

--- defense/IDS.py
+++ defense/IDS.py
@@ -1,26 +1,44 @@
 from scapy.all import *
-from scapy.layers.inet import ICMP, IP
-import time
-from multiprocessing import Process
 
-destIP = 'localhost'
-srcIP = 'localhost'
-interface = '\\Device\\NPF_Loopback'
+interface="ens3"
+Device_IP = "10.64.13.2"
+MAC_Interface = get_if_hwaddr(interface)
 
-def processPacket(packet):
+def handlePacket(packet):
+    print(packet.summary())
     if ICMP in packet:
         if Raw in packet:
-            data = packet[Raw].load.decode()
-            if "botreply" in data:
+            try:
+                data = packet[Raw].load.decode()
+            except:
+                allowPacket(packet)
                 return
-            else:
-                send(packet)
+            if "botreply" in data:
+                denyPacket(packet, "Known bot reply")
+                return
+    allowPacket(packet)
 
-def sniffForPacket():
-    sniff(iface=interface, prn=processPacket, filter='icmp')
+def allowPacket(packet):
+    if not IP in packet:
+        return
+    # Continue to next hop
+    packet[Ether].src = MAC_Interface              # MAC of this device
+    packet[Ether].dst = getmacbyip(packet[IP].dst) # MAC of destination IP
 
-if __name__ == '__main__':
-    p = Process(target=sniffForPacket)
-    p.start()
+    del packet.chksum
+    packet = packet.__class__(bytes(packet))
 
+    #packet.show2()
+    sendp(packet, iface=interface, verbose=False)
 
+def denyPacket(packet, reason):
+    print(f"\033[1;31mPacket denied: {reason}")
+    print("Details of rejected packet:")
+    packet.show()
+    print("\033[0;0m")
+
+if __name__ == "__main__":
+    print("Running IDS on interface: " + interface + " (" + MAC_Interface + ")")
+    sniffFilter = "ether dst " + MAC_Interface + " and not dst " + Device_IP
+    print("Analyzing packets with filter: " + sniffFilter)
+    sniff(iface=interface, prn=handlePacket, filter=sniffFilter)
